e30089892986177abe4fb46ca86df95a9975f0eb
YONGCACO3 <114316436+YONGCACO3@users.noreply.github.com>
2024-03-11 19:28:52 -0700

    Add files via upload

--- streamlit_chart_app.py
+++ streamlit_chart_app.py
@@ -1,32 +1,34 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import altair as alt
 
-#Sample Hist Plot
-rand=np.random.normal(1, 2, size=20)
+# Sample Hist Plot
+rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
 
-#Line Chart
-df= pd.DataFrame( np.random.randn(10, 2),    columns=['x', 'y'])
+# Line Chart
+df = pd.DataFrame(np.random.randn(10, 2),    columns=['x', 'y'])
 st.line_chart(df)
 
-#Bar Chart
-df= pd.DataFrame(    np.random.randn(10, 2),    columns=['x', 'y'])
+# Bar Chart
+df = pd.DataFrame(np.random.randn(10, 2),    columns=['x', 'y'])
 st.bar_chart(df)
 
-#Area Chart
-df= pd.DataFrame(    np.random.randn(10, 2),    columns=['x', 'y'])
+# Area Chart
+df = pd.DataFrame(np.random.randn(10, 2),    columns=['x', 'y'])
 st.area_chart(df)
 
-#Altair Chart
-df = pd.DataFrame(   np.random.randn(500, 3),   columns=['x','y','z'])
-c = alt.Chart(df).mark_circle().encode(   x='x' , y='y' , size='z', color='z', tooltip=['x', 'y', 'z'])
+# Altair Chart
+df = pd.DataFrame(np.random.randn(500, 3),   columns=['x', 'y', 'z'])
+c = alt.Chart(df).mark_circle().encode(
+    x='x', y='y', size='z', color='z', tooltip=['x', 'y', 'z'])
 st.altair_chart(c, use_container_width=True)
 
-#Display Map
-df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [49.28, -123.12],columns=['lat', 'lon'])
+# Display Map
+df = pd.DataFrame(np.random.randn(500, 2) /
+                  [50, 50] + [49.28, -123.12], columns=['lat', 'lon'])
 st.map(df)
